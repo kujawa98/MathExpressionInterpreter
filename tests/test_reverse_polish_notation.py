75222463fa08fd8a81a2bd0157cd13bd8885b59d
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-08-29 08:41:40 +0200

    test: created test class for RPN

---
+++ tests/test_reverse_polish_notation.py
@@ -0,0 +1,12 @@
+import unittest
+
+from rpn_converter import RPNConverter
+
+
+class RPNConverterTest(unittest.TestCase):
+    def setUp(self):
+        rpn_conv = RPNConverter()
+
+
+if __name__ == '__main__':
+    unittest.main()
