fd0db945d24cd2b9be41a33504b634a66f64afc9
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-08-29 09:01:43 +0200

    test(rpn): add digits only string convertion test

--- tests/test_reverse_polish_notation.py
+++ tests/test_reverse_polish_notation.py
@@ -13,6 +13,9 @@ class RPNConverterTest(unittest.TestCase):
     def test_whitespaces(self):
         self.assertEqual(self.rpn_conv.to_rpn("     \n\t\n\n\t\v\v\f\r\f\r\n    "), None)
 
+    def test_digits_only(self):
+        self.assertEqual(self.rpn_conv.to_rpn("12 3 1 43 5"), "12 3 1 43 5")
+
 
 if __name__ == '__main__':
     unittest.main()
