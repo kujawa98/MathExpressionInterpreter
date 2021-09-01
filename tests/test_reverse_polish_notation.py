28f6d5c31d791d0619eb7f4a248e1c20595892b1
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-01 11:03:17 +0200

    test(rpn): add no paren. expressions test, delete validity check test and refactor remaining tests

--- tests/test_reverse_polish_notation.py
+++ tests/test_reverse_polish_notation.py
@@ -14,18 +14,18 @@ class RPNConverterTest(unittest.TestCase):
         self.assertEqual(self.rpn_conv.to_rpn("     \n\t\n\n\t\v\v\f\r\f\r\n    "), "")
 
     def test_digits_only(self):
-        self.assertEqual(self.rpn_conv.to_rpn("12 3 1 43 5"), "")
+        self.assertEqual(self.rpn_conv.to_rpn("12 3 1 43 5"), ['12', '3', '1', '43', '5'])
 
-    def test_validity_check(self):
-        self.assertEqual(self.rpn_conv.check_validity("2 2 3 + *"), True)
-        self.assertEqual(self.rpn_conv.check_validity(""), False)
-        self.assertEqual(self.rpn_conv.check_validity("     \n\t\n\n\t\v\v\f\r\f\r\n    "), False)
-        self.assertEqual(self.rpn_conv.check_validity("2 2 3 +"), False)
-        self.assertEqual(self.rpn_conv.check_validity("2 2 2"), False)
-        self.assertEqual(self.rpn_conv.check_validity("+ * - / / + "), False)
+    def test_operators_only(self):
+        self.assertEqual(self.rpn_conv.to_rpn("* --+ //+*"), ['*', '/', '/', '*', '+', '+', '-', '-'])
+
+    def test_expressions(self):
+        self.assertEqual(self.rpn_conv.to_rpn("2 +2"), ['2', '2', '+'])
+        self.assertEqual(self.rpn_conv.to_rpn("2 +3*4"), ['2', '3', '4', '*', '+'])
 
 
 #         n + 1 operators means n operands for + - * /
+#         we want RPN expression to evaluate to single number
 
 
 if __name__ == '__main__':
