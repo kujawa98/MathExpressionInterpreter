717d7f33deb35253609992ef8f421c11f7d92f0f
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-01 11:45:14 +0200

    test(rpn): fix operators only test and characters in last assertion in expressions test

--- tests/test_reverse_polish_notation.py
+++ tests/test_reverse_polish_notation.py
@@ -17,12 +17,13 @@ class RPNConverterTest(unittest.TestCase):
         self.assertEqual(self.rpn_conv.to_rpn("12 3 1 43 5"), ['12', '3', '1', '43', '5'])
 
     def test_operators_only(self):
-        self.assertEqual(self.rpn_conv.to_rpn("* --+ //+*"), ['*', '/', '/', '*', '+', '+', '-', '-'])
+        self.assertEqual(self.rpn_conv.to_rpn("* --+ //+*"), ['*', '-', '-', '/', '/', '+', '*', '+'])
 
     def test_expressions(self):
         self.assertEqual(self.rpn_conv.to_rpn("2 +2"), ['2', '2', '+'])
         self.assertEqual(self.rpn_conv.to_rpn("2 +3*4"), ['2', '3', '4', '*', '+'])
-
+        self.assertEqual(self.rpn_conv.to_rpn("2+3*4/5-6/7+8"),
+                         ['2', '3', '4', '*', '5', '/', '+', '6', '7', '/', '-', '8', '+'])
 
 #         n + 1 operators means n operands for + - * /
 #         we want RPN expression to evaluate to single number
