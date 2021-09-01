171580fed794f2f333a05aec27984e4c10e2dea2
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-01 11:50:30 +0200

    test(lexer): create lexer test class

---
+++ tests/test_lexer.py
@@ -0,0 +1,11 @@
+import unittest
+import rpn_converter
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        rpn_conv = rpn_converter.RPNConverter()
+
+
+if __name__ == '__main__':
+    unittest.main()
