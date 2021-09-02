718bb6c256a55f7ced73c8773a819093e25c4ca0
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-02 10:09:04 +0200

    test(lexer): add empty expression test

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,10 +1,16 @@
 import unittest
 import rpn_converter
+import lexer
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        rpn_conv = rpn_converter.RPNConverter()
+        self.rpn_conv = rpn_converter.RPNConverter()
+        self.lex = lexer.Lexer()
+
+    def test_empty(self):
+        exp = self.rpn_conv.to_rpn("")
+        self.assertEqual(self.lex.tokenize(exp), [])
 
 
 if __name__ == '__main__':
