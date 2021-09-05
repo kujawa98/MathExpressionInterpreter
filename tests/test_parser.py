f7a0e890f8de51f2363e527197d8a7fdb493ed3d
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-05 18:36:54 +0200

    test(parser): create parser test class

---
+++ tests/test_parser.py
@@ -0,0 +1,11 @@
+import unittest
+from parser_ import Parser
+
+
+class ParserTest(unittest.TestCase):
+    def setUp(self):
+        parser_ = Parser()
+
+
+if __name__ == '__main__':
+    unittest.main()
