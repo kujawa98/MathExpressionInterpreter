b47067729ab56201b91608e63ab43ecce3cb59d5
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-05 09:00:51 +0200

    feat(lexer): tokenizer handles four basic operators

--- lexer.py
+++ lexer.py
@@ -8,4 +8,12 @@ class Lexer:
             for part in expression:
                 if part.isdigit():
                     result.append(Token(TokenType.NUMBER, int(part)))
+                elif part == "+":
+                    result.append(Token(TokenType.ADD))
+                elif part == "-":
+                    result.append(Token(TokenType.SUB))
+                elif part == "*":
+                    result.append(Token(TokenType.MUL))
+                elif part == "/":
+                    result.append(Token(TokenType.DIV))
         return result
