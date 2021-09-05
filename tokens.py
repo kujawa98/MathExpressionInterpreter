00adab1407692a2664fd6591c6318034b0d56674
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-05 07:39:21 +0200

    fix(lexer): change colons to assignment

--- tokens.py
+++ tokens.py
@@ -3,13 +3,13 @@ from enum import Enum
 
 
 class TokenType(Enum):
-    NUMBER: 1
-    ADD: 2
-    SUB: 3
-    MUL: 4
-    DIV: 5
-    LPAR: 6
-    RPAR: 7
+    NUMBER = 1
+    ADD = 2
+    SUB = 3
+    MUL = 4
+    DIV = 5
+    LPAR = 6
+    RPAR = 7
 
 
 @dataclass
