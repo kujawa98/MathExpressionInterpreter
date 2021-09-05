d51f4af140e33e7690b62e86df9d4b46b4f5bf03
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-05 19:10:43 +0200

    test(parser): add one number parse test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,7 @@
 import unittest
 from parser_ import Parser
+from tokens import *
+from nodes import *
 
 
 class ParserTest(unittest.TestCase):
@@ -9,6 +11,17 @@ class ParserTest(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(self.parser_.parse([]), None)
 
+    def test_one_number(self):
+        tk = [Token(TokenType.NUMBER, 9)]
+        res_node = self.parser_.parse(tk)
+        nd = Node(NodeType.NUMBER, None, None, tk[0].value)
+        self.assertIsInstance(res_node, Node)
+        self.assertEqual(res_node.type, NodeType.NUMBER)
+        self.assertEqual(res_node.value, nd.value)
+
+
+#         check if there's number in a first place, if so, check if there' any tokens left
+#         if so expression is invalid and return None, if not it is OK
 
 if __name__ == '__main__':
     unittest.main()
