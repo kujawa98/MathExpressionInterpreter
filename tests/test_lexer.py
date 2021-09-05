c60798cbe8524cc6bbda37edbae08a65c1884a5e
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-05 07:37:46 +0200

    test(lexer): add digits only test

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,6 +1,7 @@
 import unittest
 import rpn_converter
 import lexer
+from tokens import TokenType
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,6 +16,15 @@ class MyTestCase(unittest.TestCase):
     def test_whitespaces(self):
         exp = self.rpn_conv.to_rpn("   \t\n\n\v\r\f\n\t     ")
         self.assertEqual(self.lex.tokenize(exp), [])
+
+    def test_digits_only(self):
+        exp = self.rpn_conv.to_rpn(" 26  4 8 ")
+        res = self.lex.tokenize(exp)
+        tkns = [TokenType.NUMBER, TokenType.NUMBER, TokenType.NUMBER]
+        self.assertEqual(len(res), len(tkns))
+        for i in range(len(tkns)):
+            self.assertEqual(res[i].type, tkns[i])
+
 
 if __name__ == '__main__':
     unittest.main()
