be601b34e5521380746b4e65018cc2b2d03c0338
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-06 15:02:00 +0200

    feat(parser): parser handles numbers only

--- parser_.py
+++ parser_.py
@@ -1,8 +1,12 @@
 from nodes import *
+from tokens import *
+
 
 class Parser:
     def parse(self, tokens):
+        if len(tokens) == 1 and tokens[0].type == TokenType.NUMBER:
+            return Node(NodeType.NUMBER, None, None, tokens[0].value)
         main_node = None
         if tokens:
-            pass
-        return None
+            return
+        return
