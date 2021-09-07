44c74ec7006febff7c37a33ebf12595611c813b8
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-07 11:49:57 +0200

    feat(interpreter): implement interpreter

--- interpreter.py
+++ interpreter.py
@@ -6,4 +6,17 @@ class Interpreter:
         if ast:
             if ast.type == NodeType.NUMBER:
                 return ast.value
+            return self.get_value(ast)
         return ""
+
+    def get_value(self, node):
+        if node.type == NodeType.ADD:
+            return self.get_value(node.right_child) + self.get_value(node.left_child)
+        elif node.type == NodeType.SUB:
+            return self.get_value(node.right_child) - self.get_value(node.left_child)
+        elif node.type == NodeType.MUL:
+            return self.get_value(node.right_child) * self.get_value(node.left_child)
+        elif node.type == NodeType.DIV:
+            return self.get_value(node.right_child) // self.get_value(node.left_child)
+        elif node.type == NodeType.NUMBER:
+            return node.value
