00babb026e333246cb1cccbfe043b0357acb72af
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-07 09:07:30 +0200

    feat(parser): parser handles simple expressions

--- parser_.py
+++ parser_.py
@@ -4,9 +4,33 @@ from tokens import *
 
 class Parser:
     def parse(self, tokens):
-        if len(tokens) == 1 and tokens[0].type == TokenType.NUMBER:
-            return Node(NodeType.NUMBER, None, None, tokens[0].value)
         main_node = None
         if tokens:
+            main_node = self.create_node(tokens.pop(), tokens)
+        if len(tokens) != 0:
+            print("Semantic error")
             return
-        return
+        return main_node
+
+    def create_node(self, current_token, tokens):
+        try:
+            if current_token.type == TokenType.ADD:
+                l_child = self.create_node(tokens.pop(), tokens)
+                r_child = self.create_node(tokens.pop(), tokens)
+                return Node(NodeType.ADD, l_child, r_child)
+            elif current_token.type == TokenType.SUB:
+                l_child = self.create_node(tokens.pop(), tokens)
+                r_child = self.create_node(tokens.pop(), tokens)
+                return Node(NodeType.SUB, l_child, r_child)
+            elif current_token.type == TokenType.MUL:
+                l_child = self.create_node(tokens.pop(), tokens)
+                r_child = self.create_node(tokens.pop(), tokens)
+                return Node(NodeType.MUL, l_child, r_child)
+            elif current_token.type == TokenType.DIV:
+                l_child = self.create_node(tokens.pop(), tokens)
+                r_child = self.create_node(tokens.pop(), tokens)
+                return Node(NodeType.DIV, l_child, r_child)
+            elif current_token.type == TokenType.NUMBER:
+                return Node(NodeType.NUMBER, None, None, current_token.value)
+        except IndexError:
+            return
