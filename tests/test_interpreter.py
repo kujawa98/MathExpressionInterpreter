58a7c58d5bfa7b2c19f971137021206be26c7e4e
Kacper Kujawa <kacper.kujawa98@gmail.com>
2021-09-21 10:25:02 +0200

    test(interpreter): refactor tests

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -8,39 +8,30 @@ from rpn_converter import RPNConverter
 
 class InterpreterTest(unittest.TestCase):
     def setUp(self):
-        self.parser = Parser()
-        self.lexer = Lexer()
-        self.rpn = RPNConverter()
         self.interpreter = Interpreter()
 
     def test_empty(self):
-        ppp = self.parser.parse(self.lexer.tokenize(self.rpn.to_rpn("")))
-        res = self.interpreter.interpret(ppp)
+        res = self.interpreter.execute("")
         self.assertEqual(res, "")
 
     def test_one_number(self):
-        ppp = self.parser.parse(self.lexer.tokenize(self.rpn.to_rpn("24")))
-        res = self.interpreter.interpret(ppp)
+        res = self.interpreter.execute("24")
         self.assertEqual(res, 24)
 
     def test_more_than_one_number(self):
-        ppp = self.parser.parse(self.lexer.tokenize(self.rpn.to_rpn("24 31 54")))
-        res = self.interpreter.interpret(ppp)
+        res = self.interpreter.execute("24 31 54")
         self.assertEqual(res, "")
 
     def test_operators(self):
-        ppp = self.parser.parse(self.lexer.tokenize(self.rpn.to_rpn("*-+/")))
-        res = self.interpreter.interpret(ppp)
+        res = self.interpreter.execute("*-+/")
         self.assertEqual(res, "")
 
     def test_simple_exp(self):
-        ppp = self.parser.parse(self.lexer.tokenize(self.rpn.to_rpn("2+2")))
-        res = self.interpreter.interpret(ppp)
+        res = self.interpreter.execute("2+2")
         self.assertEqual(res, 4)
 
     def test_exp(self):
-        ppp = self.parser.parse(self.lexer.tokenize(self.rpn.to_rpn("2+3*4+10/5")))
-        res = self.interpreter.interpret(ppp)
+        res = self.interpreter.execute("2+3*4+10/5")
         self.assertEqual(res, 16)
 
 
